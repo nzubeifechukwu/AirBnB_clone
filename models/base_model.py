8c7b281bc968a34cea45e4ff240c432cc6368528
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-02-07 13:59:30 -0800

    Updating modules

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 from datetime import datetime
 import uuid
+from models.__init__ import storage
 
 
 class BaseModel:
@@ -19,7 +20,7 @@ class BaseModel:
                 every time the object changes
         '''
         if kwargs != {}:
-            for k,v in kwargs.items():
+            for k, v in kwargs.items():
                 if k != '__class__':
                     if k == 'created_at' or k == 'updated_at':
                         setattr(self, k, datetime.fromisoformat(v))
@@ -29,6 +30,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         '''Demonstrates how to print the object
@@ -41,6 +43,7 @@ class BaseModel:
             with the current datetime
         '''
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         '''Returns a dictionary containing all keys/values of
