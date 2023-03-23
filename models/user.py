c60bdf65dbed4b6a877c7f7a018a4b1e6c2da4b7
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-03-23 08:06:32 -0700

    Update models/user.py

--- models/user.py
+++ models/user.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 # User class inherits from BaseModel
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     '''Defines a User class that inherits from BaseModel class
 
     Attributes:
@@ -13,7 +14,8 @@ class User(BaseModel):
         first_name: user first name
         last_name: user last name
     '''
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ ='users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
